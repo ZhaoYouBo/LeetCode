6234a4d7af550c149153d110245799c79ec4cf09
ZhaoRongBo <ouyangyilong427@gmail.com>
2025-07-21 17:15:56 +0800

    Add code_1_two_sum.py

---
+++ python/code_15_three_sum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    @staticmethod
+    def three_sum(nums: List[int]) -> List[List[int]]:
+        """
+
+        :param nums:
+        :return:
+        """
+        nums.sort() # 排序
--- python/code_1_two_sum.py
+++ python/code_1_two_sum.py
@@ -10,27 +10,19 @@ class Solution:
         :param target: 整数，表示目标和
         :return: 包含两个索引的列表，这两个数的和等于目标值；如果没有符合条件的数对，返回空列表
         """
-        # 外层循环遍历数组中的每一个元素
-        for i in range(len(nums)):
-            # 内层循环从当前元素的下一个元素开始遍历
-            for j in range(i+1, len(nums)):
+        for i in range(len(nums)): # 外层循环遍历数组中的每一个元素
+            for j in range(i+1, len(nums)): # 内层循环从当前元素的下一个元素开始遍历
                 if nums[i] + nums[j] == target:
                     return [i,j]
         return []
 
 def test():
-    # 标准情况
-    assert Solution.two_sum([2,7,11,15], 9) == [0,1]
-    # 包含负数
-    assert Solution.two_sum([-1, -2, -3, -4], -7) == [2,3]
-    # 多个解，只需要返回一个
-    assert Solution.two_sum([3, 2, 4, 3], 6) in [[0,3], [2,1]]
-    # 无解的情况
-    assert Solution.two_sum([1, 2, 3], 10) == []
-    # 只有两个元素的情况
-    assert Solution.two_sum([5, 5], 10) == [0,1]
-    # 边界情况，空列表或只有一个元素
-    assert Solution.two_sum([], 0) == []
+    assert Solution.two_sum([2,7,11,15], 9) == [0,1] # 标准情况
+    assert Solution.two_sum([-1, -2, -3, -4], -7) == [2,3] # 包含负数
+    assert Solution.two_sum([3, 2, 4, 3], 6) in [[0,3], [2,1]] # 多个解，只需要返回一个
+    assert Solution.two_sum([1, 2, 3], 10) == [] # 无解的情况
+    assert Solution.two_sum([5, 5], 10) == [0,1] # 只有两个元素的情况
+    assert Solution.two_sum([], 0) == [] # 边界情况，空列表或只有一个元素
     assert Solution.two_sum([1], 1) == []
 
     print("所有测试通过！")
